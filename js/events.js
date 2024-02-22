11573479df770dbbc97e47793e7806c3ff163e2f
mdshakil04 <m.shakil04@yahoo.com>
2024-02-22 21:16:02 +0600

    Another onclick function done

--- js/events.js
+++ js/events.js
@@ -1,1 +1,11 @@
 console.log('JS Dom Events Running')
+function changeColor(){
+    document.body.style.backgroundColor = "aquamarine"
+}
+const tomatoBtn = document.getElementById('tomato');
+tomatoBtn.onclick = makeTomato;
+
+function makeTomato(){
+    document.body.style.backgroundColor = 'tomato';
+    alert('Your body color will be change into Tomato')
+}
