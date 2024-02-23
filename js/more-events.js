497ebc17f2400fcd599f2905a318ff72a32578e3
mdshakil04 <m.shakil04@yahoo.com>
2024-02-23 21:06:07 +0600

    More events practices

---
+++ js/more-events.js
@@ -0,0 +1,12 @@
+document.getElementById('post-btn').addEventListener('click', function(){
+    // console.log('Button generated')
+    const commentBox = document.getElementById('input-field');
+    // console.log(commentBox)
+    const newComment = commentBox.value;
+    // console.log(newComment);
+    const commentArea = document.getElementById('comment-container');
+    const p = document.createElement('p');
+    p.innerText = newComment;
+    commentArea.appendChild(p)
+    commentBox.value = '';
+})
